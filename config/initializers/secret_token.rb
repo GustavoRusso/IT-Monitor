fc050f50666ce9e94ac41498729c50c2762ab366
Gustavo Russo <gustavo.russo@gmail.com>
2012-09-07 16:31:43 -0500

    created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItScreen::Application.config.secret_token = 'dca55baa81b238d7da374ff67317a6b9930744c52dac557ebec3d3e22cf86077d4df4a01e5f3fab65273ccf17465b20e6515a58aa1ce5e288574cf43bacd8e4f'
